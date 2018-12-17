f9428ff132c7f7b1f7fc40f1b9cd8ef2de5eb243
= <=>
2018-12-17 14:18:30 +0700

    first commit

---
+++ unit_41/unit_41.py
@@ -0,0 +1,18 @@
+from tkinter import*
+
+class Window(Frame):
+    def __init__(self,master = None):
+        Frame.__init__(self,master)
+        self.master = master
+        self.init_window()
+    def init_window(self):
+        self.master.title("GUI")
+        self.pack(fill = BOTH,expand = 1)
+        quitButton = Button(self, text = "Quit")
+        quitButton.place(x=0,y=0)
+        enterButton = Button(self,text = "Enter")
+        enterButton.place(x=160,y=0)
+root = Tk()
+root.geometry("200x200")
+app = Window(root)
+root.mainloop()
