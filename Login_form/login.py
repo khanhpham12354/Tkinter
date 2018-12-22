38d95afb77a86f714c436cfc6642507456a2373a
= <=>
2018-12-22 16:55:56 +0700

    fourth commit

--- Login_form/login.py
+++ Login_form/login.py
@@ -1,25 +1,26 @@
 from tkinter import*
 from tkinter import messagebox
 import os
-data = {'pham duc khanh':'123456','dinh duy nam':'123321'}
+import sqlite3 as sql
+data = {}
 
-# intit login form
 def login_init():
 	global app;
 	app = Tk();
 	app.title("Login");
-	app.geometry("400x150");
+	app.geometry("400x150+420+270");
 	app.resizable(height= False,width = False);
 	app.iconbitmap("photos/lcd.ico");
 def quit():
-	exit();
+	 x = messagebox.askokcancel("Quit","Do you want to quit?");
+	 if(x==True):
+	 	exit();
 def login_susscues():
 	user_name = user.get().lower();
 	pwd = password.get().lower();
 	if user_name in data and data[user_name]==pwd:
 		messagebox.showinfo("Success","Successfully!!!")
 		app.destroy();
-		os.system("python lcd.py");
 	else:
 		if user_name not in data:
 			messagebox.showerror("Error","Incorrect Username!");
@@ -31,7 +32,7 @@ def login_setup():
 	password = StringVar();
 	frame_1 = Frame(app);
 	frame_1.pack();
-	lbl_user = Label(frame_1,text = "User:",bg = "white",fg= "black",font=("Times",16,'bold'),);
+	lbl_user = Label(frame_1,text = "User:",bg = "white",fg= "black",font=("Times",16,'bold'));
 	lbl_pass = Label(frame_1,text = "Password:",bg="white",fg = "black",font=("Times",16,'bold'));
 
 	etn_user = Entry(frame_1,font=("Times",16,'bold'),justify= RIGHT,textvariable = user);
@@ -53,8 +54,21 @@ def login_setup():
 	btn_login.grid(row = 0,column = 0);
 	frame_3.grid(row = 0,column = 1)
 	btn_quit.grid(row = 0,column = 2);
+# intit database
+def data_init():
+	global data;
+	path  = os.path.dirname(__file__) + "\\data_user.db";
+	con = sql.connect(path);
+	with con:
+		cur  = con.cursor();
+		cur.execute("SELECT* FROM customer");
+		rows = cur.fetchall();
+		for i in range(0,len(rows)):
+			data[rows[i][1]] = rows[i][2];
+		#print(data)
 
 if __name__ == '__main__':
+	data_init();
 	login_init();
 	login_setup();
-	app.mainloop();
+	mainloop();
